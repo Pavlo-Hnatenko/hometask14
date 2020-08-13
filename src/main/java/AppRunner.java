3a4939ffe2cbbe5c9b3c7b353c32a1f71171c602
Pavlo-Hnatenko <gnatenko.pavlo@gmail.com>
2020-08-13 10:50:19 +0300

    seventh commit: class AppRunner was modified

--- src/main/java/AppRunner.java
+++ src/main/java/AppRunner.java
@@ -13,118 +13,26 @@ import java.util.Properties;
 public class AppRunner {
 
     private static final Logger log = LoggerFactory.getLogger(AppRunner.class);
+    static Properties props = loadProperties();
+    static String url = props.getProperty("url");
+    static Connection connection;
+
+    static {
+        try {
+            connection = DriverManager.getConnection(url, props);
+            log.info("Connecting to {}", url);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) {
 
-        Properties props = loadProperties();
-        String url = props.getProperty("url");
-        log.info("Connecting to {}", url);
+            addLocations();
+            addRoutes();
+            addProblems();
+            setSolutions(getCostSet());
 
-        try (Connection connection = DriverManager.getConnection(url, props)) {
-
-            try (PreparedStatement getAllLocations = connection.prepareStatement(
-                    "SELECT name FROM locations")) {
-
-                ResultSet resultSet = getAllLocations.executeQuery();
-
-                while (resultSet.next()) {
-
-                    String name = resultSet.getString("name");
-                    log.info("Location is founded. Name: {}", name);
-                }
-            }
-
-            Location[] locations = {
-                    new Location("gdansk"),
-                    new Location("bydgoszcz"),
-                    new Location("torun"),
-                    new Location("warszawa")
-            };
-
-            try (PreparedStatement insertLocation = connection.prepareStatement(
-
-                    "INSERT INTO locations (name) VALUES (?) ON CONFLICT DO NOTHING;",
-                    PreparedStatement.RETURN_GENERATED_KEYS
-            )) {
-
-                for (Location location : locations) {
-                    insertLocation.setString(1, location.getName());
-                    insertLocation.addBatch();
-                }
-
-                insertLocation.executeBatch();
-                ResultSet generatedKeys = insertLocation.getGeneratedKeys();
-
-                while (generatedKeys.next()) {
-                    log.info("inserted new location. ID : {}", generatedKeys.getLong("id"));
-                }
-            }
-
-            Route[] routes = {
-                    new Route(1, 2, 1),
-                    new Route(1, 3, 3),
-                    new Route(2, 1, 1),
-                    new Route(2, 3, 1),
-                    new Route(2, 4, 4),
-                    new Route(3, 1, 3),
-                    new Route(3, 2, 1),
-                    new Route(3, 4, 1),
-                    new Route(4, 2, 4),
-                    new Route(4, 3, 1),
-
-            };
-
-            try (PreparedStatement insertRoute = connection.prepareStatement(
-
-                    "INSERT INTO routes (from_id, to_id, cost) VALUES (?, ?, ?) ON CONFLICT DO NOTHING;",
-                    PreparedStatement.RETURN_GENERATED_KEYS
-            )) {
-
-                for (Route route : routes) {
-                    insertRoute.setInt(1, route.getFromId());
-                    insertRoute.setInt(2, route.getToId());
-                    insertRoute.setInt(3, route.getCost());
-
-                    insertRoute.addBatch();
-                }
-
-                insertRoute.executeBatch();
-                ResultSet generatedKeys = insertRoute.getGeneratedKeys();
-
-                while (generatedKeys.next()) {
-                    log.info("inserted new route. ID : {}", generatedKeys.getLong("id"));
-                }
-            }
-
-            Problem[] problems = {
-                    new Problem(1, 4),
-                    new Problem(2, 4)
-            };
-
-            try (PreparedStatement insertProblem = connection.prepareStatement(
-
-                    "INSERT INTO problems (from_id, to_id) VALUES (?, ?) ON CONFLICT DO NOTHING;",
-                    PreparedStatement.RETURN_GENERATED_KEYS
-            )) {
-
-                for (Problem problem : problems) {
-                    insertProblem.setInt(1, problem.getFromId());
-                    insertProblem.setInt(2, problem.getToId());
-
-                    insertProblem.addBatch();
-                }
-
-                insertProblem.executeBatch();
-                ResultSet generatedKeys = insertProblem.getGeneratedKeys();
-
-                while (generatedKeys.next()) {
-                    log.info("inserted new problem. ID : {}", generatedKeys.getLong("id"));
-                }
-            }
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
     }
 
     private static Properties loadProperties() {
@@ -138,6 +46,157 @@ public class AppRunner {
         }
 
         return props;
+    }
+
+    private static void addLocations() {
+
+        Location[] locations = {
+                new Location("gdansk"),
+                new Location("bydgoszcz"),
+                new Location("torun"),
+                new Location("warszawa")
+        };
+
+        try (PreparedStatement insertLocation = connection.prepareStatement(
+                "INSERT INTO locations (name) VALUES (?) ON CONFLICT DO NOTHING;"
+        )) {
+
+            for (Location location : locations) {
+                insertLocation.setString(1, location.getName());
+                insertLocation.addBatch();
+            }
+
+            insertLocation.executeBatch();
+            ResultSet generatedKeys = insertLocation.getGeneratedKeys();
+
+            while (generatedKeys.next()) {
+                log.info("inserted new location. ID : {}", generatedKeys.getLong("id"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void addRoutes() {
+
+        Route[] routes = {
+                new Route(1, 2, 1),
+                new Route(1, 3, 3),
+                new Route(2, 1, 1),
+                new Route(2, 3, 1),
+                new Route(2, 4, 4),
+                new Route(3, 1, 3),
+                new Route(3, 2, 1),
+                new Route(3, 4, 1),
+                new Route(4, 2, 4),
+                new Route(4, 3, 1),
+
+        };
+
+        try (PreparedStatement insertRoute = connection.prepareStatement(
+                "INSERT INTO routes (from_id, to_id, cost) VALUES (?, ?, ?) ON CONFLICT DO NOTHING;"
+        )) {
+
+            for (Route route : routes) {
+                insertRoute.setInt(1, route.getFromId());
+                insertRoute.setInt(2, route.getToId());
+                insertRoute.setInt(3, route.getCost());
+
+                insertRoute.addBatch();
+            }
+
+            insertRoute.executeBatch();
+            ResultSet generatedKeys = insertRoute.getGeneratedKeys();
+
+            while (generatedKeys.next()) {
+                log.info("inserted new route. ID : {}", generatedKeys.getLong("id"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void addProblems() {
+
+        Problem[] problems = {
+                new Problem(1, 4),
+                new Problem(2, 4)
+        };
+
+        try (PreparedStatement insertProblem = connection.prepareStatement(
+                "INSERT INTO problems (from_id, to_id) VALUES (?, ?) ON CONFLICT DO NOTHING;"
+        )) {
+
+            for (Problem problem : problems) {
+                insertProblem.setInt(1, problem.getFromId());
+                insertProblem.setInt(2, problem.getToId());
+
+                insertProblem.addBatch();
+            }
+
+            insertProblem.executeBatch();
+            ResultSet generatedKeys = insertProblem.getGeneratedKeys();
+
+            while (generatedKeys.next()) {
+                log.info("inserted new problem. ID : {}", generatedKeys.getLong("id"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static ResultSet getCostSet() {
+
+        ResultSet costSet = null;
+
+        try (PreparedStatement selectProblem = connection.prepareStatement(
+                "SELECT * FROM problems"
+        )) {
+
+            ResultSet problemSet = selectProblem.executeQuery();
+
+            while (problemSet.next()) {
+
+                int fromId = problemSet.getInt("from_id");
+                int toId = problemSet.getInt("to_id");
+
+                try (PreparedStatement findRoutes = connection.prepareStatement(
+                        "SELECT cost FROM routes WHERE from_id = ? AND to_id = ? ON CONFLICT DO NOTHING;"
+                )) {
+
+                    findRoutes.setInt(1, fromId);
+                    findRoutes.setInt(2, toId);
+
+                    findRoutes.addBatch();
+
+                    costSet = findRoutes.executeQuery();
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return costSet;
+    }
+
+    private static void setSolutions(ResultSet costSet) {
+
+        while (true) {
+            try {
+                if (!costSet.next()) break;
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+            try (PreparedStatement insertSolutions = connection.prepareStatement(
+                    "INSERT INTO solutions (cost) VALUES (?) ON CONFLICT DO NOTHING;"
+            )) {
+                insertSolutions.setInt(1, costSet.getInt("cost"));
+
+                insertSolutions.executeUpdate();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 }
